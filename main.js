eb06f9c890fca870d9d8819b2216ae4dd025f4d5
Gulsen Yilmaz <gsenol@gmail.com>
2014-03-31 15:34:16 -0700

    created grid view and wrote initial random colors/names logic

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-angular.module('ColorChallenge', [])
-.controller('Main', function($scope){
-	console.log('app created!');
-	$scope.person = {
-		name: 'Gulsen'
-	};
+$(document).ready(function(){
+	console.log()
+
+	var colors = ['red','blue', 'green', 'yellow', 'teal', 'purple', 'pink', 'olive', 'gray'];
+	var colorCodes = [
+		['red', '#ff0000'], ['blue', '#0000ff'], ['green', '#00ff00'],
+		['yellow', '#ffff00'], ['teal', '#008080'], ['purple', '#800080'], 
+		['pink', '#ff00ff'], ['olive', '#808000'], ['gray', '#808080']
+	];
+
+	var $tile = $('.tile');
+	$tile.each(function(color){
+		var random1 = Math.floor(Math.random() *  colors.length);;
+		var random2 = Math.floor(Math.random() *  colors.length);;
+		$(this).css('background-color', colorCodes[random1][1]).text(colors[random2]);
+	});
 });
+
