0be6c420913a371e8e4c390160731ec1cacc55ca
Gulsen Yilmaz <gsenol@gmail.com>
2014-03-31 16:53:08 -0700

    changed design to make all tiles the same color and enabled text and color comparison on clicked tiles

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 $(document).ready(function(){
-	console.log()
+	var score = 0;
 
-	var colors = ['red','blue', 'green', 'yellow', 'teal', 'purple', 'pink', 'olive', 'gray'];
-	var colorCodes = [
-		['red', '#ff0000'], ['blue', '#0000ff'], ['green', '#00ff00'],
-		['yellow', '#ffff00'], ['teal', '#008080'], ['purple', '#800080'], 
-		['pink', '#ff00ff'], ['olive', '#808000'], ['gray', '#808080']
-	];
+	var colors = ['red','blue', 'green', 'yellow', 'purple', 'pink'];
+	var colorCodes = ['#ff0000','#0000ff', '#00ff00', '#ffff00', '#800080', '#ff00ff'];
+	var colorsLib = {
+		red: '255, 0, 0', blue: '0, 0, 255', green: '0, 255, 0', 
+		yellow: '255, 255, 0', purple: '128, 0, 128', pink: '255, 0, 255'
+	};
+
+	var generateTiles = function(){
+		var indexColor = Math.floor(Math.random() *  colors.length);
+		var indexCode = Math.floor(Math.random() *  colorCodes.length);
+		return [colorCodes[indexCode], colors[indexColor]]
+	};
+
 
 	var $tile = $('.tile');
-	$tile.each(function(color){
-		var random1 = Math.floor(Math.random() *  colors.length);;
-		var random2 = Math.floor(Math.random() *  colors.length);;
-		$(this).css('background-color', colorCodes[random1][1]).text(colors[random2]);
+	$tile.each(function(){
+		var input = generateTiles();
+		$(this).text(input[1]).css('color', input[0]);
+	});
+
+	$tile.on('click', function(){
+		var text = $(this).text();
+		var color = $(this).css('background-color').toString();
+		console.log(color, 'rgb(' + colorsLib[text] + ')');
+		if(color === 'rgb(' + colorsLib[text] + ')') {
+			console.log('Success!');
+		} else {
+			console.log('nope!');
+		}
 	});
 });
 
